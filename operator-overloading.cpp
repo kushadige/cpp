291692ffce019b5f72696df45f143011f9f6aa53
oguzhankuslar <101992799+oguzhankuslar@users.noreply.github.com>
2022-04-08 10:09:46 +0300

    commit

--- operator-overloading.cpp
+++ operator-overloading.cpp
@@ -12,6 +12,7 @@ public:
     RasyonelSayi operator+(const RasyonelSayi &rs);
     std::string toString();
     friend std::ostream& operator<<(std::ostream &out, RasyonelSayi &rs);
+    friend void operator>>(std::istream &in, RasyonelSayi &rs);
 };
 
 std::ostream& operator<<(std::ostream &out, RasyonelSayi &rs){
@@ -28,6 +29,12 @@ int main(){
     // "+" overloading
     RasyonelSayi sum = s1 + s2;
     cout << sum;
+
+    RasyonelSayi s3;
+    // ">>" overloading
+    cin >> s3;
+    
+    cout << "rasyonel sayi: " << s3;
 }
 //class functions
 RasyonelSayi::RasyonelSayi(){};
@@ -42,4 +49,34 @@ RasyonelSayi RasyonelSayi::operator+(const RasyonelSayi &rs){
 
 std::string RasyonelSayi::toString(){
     return std::to_string(pay) + "/" + std::to_string(payda);
+}
+
+void operator>>(std::istream &in, RasyonelSayi &rs){
+    /* --if you want to ask numerator and denominator you can use this code below.
+    int x,y;
+    
+    cout <<"pay: ";
+    in >> x;
+    cout <<"payda: ";
+    in >> y;
+    
+    rs.pay = x;
+    rs.payda = y;
+    */
+    // --also if you want to get inputs like "3/4" then you can use this--
+    std::string num; //3/4
+
+    in >> num;
+
+    int index = num.find('/'); //the method "find" returns integer or you can use size_t also..
+    
+    if(index == 0){ //if you used size_t type above you should replace condition to ( index == std::string::npos )
+        rs.pay = std::stoi(num);
+        rs.payda = 1;
+        
+        return; // exits function if the condition is valid
+    }
+      
+    rs.pay = std::stoi( num.substr(0,index) );
+    rs.payda = std::stoi( num.substr(index+1) );
 }
